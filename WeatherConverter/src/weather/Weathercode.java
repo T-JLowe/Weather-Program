c208bbeab3f02a43ac2593c17b233e6abfdf886f
T-JLowe <31040396+T-JLowe@users.noreply.github.com>
2017-08-22 08:39:33 -0500

    Add files via upload

---
+++ WeatherConverter/src/weather/Weathercode.java
@@ -0,0 +1,31 @@
+package weather;
+import java.util.Scanner;
+
+public class Weathercode 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Enter the temperature in fahrenheit");
+
+		Scanner cin = new Scanner(System.in);
+
+		float Fahr = cin.nextFloat();
+		float Celc = 0;
+
+		Celc = Fahr - 32;
+		Celc = Celc * 5/9;
+		
+		if(Fahr<32)
+		{
+			System.out.println("Brrr! It sure is cold outside at " + Celc + " degrees centrigrade");
+		}
+		else if(Fahr>90)
+		{
+			System.out.println("Whew! It sure is hot outside at " + Celc + " degrees centigrade");
+		}
+		else
+		{
+			System.out.println("Outside, it is " + Celc + " degrees centigrade");
+		}
+	}
+}
